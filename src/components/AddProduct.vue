1d885a8547d1960473550a6384861439f8618ebd
Tchander <evgeniy.tchander@yandex.ru>
2021-10-10 18:47:48 +0300

    feat: add flex and styles to ProductForm title

--- src/components/AddProduct.vue
+++ src/components/AddProduct.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <div class="ida-product__title"></div>
+  <div class="ida-product">
+    <div class="ida-product__title">Добавление товара</div>
     <add-product-form />
   </div>
 </template>
@@ -13,4 +13,15 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ida-product {
+  width: 100%;
+  max-width: 332px;
+}
+.ida-product__title {
+  color: #3f3f3f;
+  font-size: 28px;
+  font-weight: 600;
+  line-height: 35.2px;
+}
+</style>
